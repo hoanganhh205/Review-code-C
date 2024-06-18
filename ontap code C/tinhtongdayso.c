b905727c01bea5cd8ae241ecc6c5360873faf8a5
hoanganhh205 <anh01082k5@gmail.com>
2024-06-19 00:13:15 +0700

    f

--- ontap code C/tinhtongdayso.c
+++ ontap code C/tinhtongdayso.c
@@ -1,7 +1,7 @@
 //Tính tổng dãy số: Viết chương trình tính tổng dãy số từ 1 đến n.
 #include <stdio.h>
 int main() 
-{
+{ 
     int n, t = 0;
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) 
